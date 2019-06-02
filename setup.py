ef03e93b13dd3d6ba3c6c1bfbbf87af1c9f23579
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-06-02 19:32:47 +0200

    Uploaded new setup?

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import re
 import sys
 # To use a consistent encoding
-from codecs import open
+from codecs import open as copen
 from os import path
 
 from setuptools import find_packages, setup
@@ -10,12 +10,12 @@ from setuptools import find_packages, setup
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with copen(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 
 def read(*parts):
-    with open(os.path.join(here, *parts), 'r') as fp:
+    with copen(os.path.join(here, *parts), 'r') as fp:
         return fp.read()
 
 
@@ -30,7 +30,7 @@ def find_version(*file_paths):
 
 __version__ = find_version("extra_keras_metrics", "__version__.py")
 
-test_deps = ['pytest', 'pytest-cov', 'coveralls', 'numpy', "silence_tensorflow"]
+test_deps =["pytest", "codacy-coverage", "coveralls", "validate_version_code", "silence_tensorflow", "pytest-cov", "numpy"]
 
 extras = {
     'test': test_deps,
@@ -38,51 +38,23 @@ extras = {
 
 setup(
     name='extra_keras_metrics',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
     version=__version__,
-
-    description='Additional metrics integrated with the keras NN library, taken directly from tensorflow.',
+    description="Additional metrics integrated with the keras NN library, taken directly from `Tensorflow <https://www.tensorflow.org/api_docs/python/tf/metrics/>`_",
     long_description=long_description,
-
-    # The project's main homepage.
-    url='https://github.com/LucaCappelletti94/extra_keras_metrics',
-
-    # Author details
-    author='Luca Cappelletti',
-    author_email='cappelletti.luca94@gmail.com',
-
+    url="https://github.com/LucaCappelletti94/extra_keras_metrics",
+    author="Luca Cappelletti",
+    author_email="cappelletti.luca94@gmail.com",
     # Choose your license
     license='MIT',
-
     include_package_data=True,
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
-
-        # Pick your license as you wish (should match "license" above)
+        'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3'
     ],
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
     tests_require=test_deps,
+    # Add here the package dependencies
     install_requires=["keras", "decorator"],
     extras_require=extras,
 )
