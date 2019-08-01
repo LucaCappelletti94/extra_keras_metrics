bfe9c16fd7db394b5f5bc27c1a575ccbbb0b5269
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-08-01 13:59:23 +0200

    Extended testing

---
+++ tests/test_keras_injection.py
@@ -0,0 +1,21 @@
+from keras import Sequential
+from keras.layers import Dense
+import extra_keras_metrics
+import numpy as np
+
+
+def test_keras_injection():
+    model = Sequential([
+        Dense(1, activation="sigmoid")
+    ])
+
+    model.compile(
+        optimizer="nadam",
+        loss="binary_crossentropy",
+        metrics=["auprc", "accuracy"]
+    )
+
+    model.fit(
+        *np.random.randint(2, size=(2, 1000, 1)),
+        verbose=0
+    )
--- tests/utils/compare_metrics.py
+++ tests/utils/compare_metrics.py
@@ -2,11 +2,10 @@ from typing import Callable, Dict
 import numpy as np
 from .run_metric import run_metric
 
-def compare_metrics(tensorflow_metric:Callable, baseline_metric:Callable, baseline_metric_kwargs:Dict=None, tests:int=10, min_pearson_correlation:float=0.99):
+def compare_metrics(tensorflow_metric:Callable, baseline_metric:Callable, tests:int=10, min_pearson_correlation:float=0.99):
     """Run comparison test for the two given metrics.
         tensorflow_metric:Callable, metric to be tested.
         baseline_metric:Callable, metric to use as baseline.
-        baseline_metric_kwargs:Dict=None, arguments to pass to the baseline metric.
         tests:int=20, number of test runs.
         min_pearson_correlation:float=0.01, minimum pearson correlation.
     """
@@ -15,21 +14,12 @@ def compare_metrics(tensorflow_metric:Callable, baseline_metric:Callable, baseli
     N = 100000
     y_pred = np.random.randint(N, size=size)/N
 
-    if baseline_metric_kwargs is None:
-        baseline_metric_kwargs = {}
-
     scores = np.array([
         (
             run_metric(tensorflow_metric, y_true[i], y_pred[i]),
-            baseline_metric(y_true[i], y_pred[i], **baseline_metric_kwargs)
+            baseline_metric(y_true[i], y_pred[i])
         ) for i in range(tests)
     ]).T
     pearson = np.corrcoef(scores).ravel()[1]
 
-    try:
-        assert pearson >= min_pearson_correlation
-    except AssertionError as e:
-        print("Test for {metric} failed!".format(metric=tensorflow_metric.__name__))
-        print("Score: {corr}".format(corr=pearson))
-        print(scores)
-        raise e
+    assert pearson >= min_pearson_correlation
