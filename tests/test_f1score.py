d95fe7ca08fc2f986c23e1653123b947c3b26fbf
zommiommy <tommaso.fontana.96@gmail.com>
2021-01-04 19:39:13 +0100

    added some tests

---
+++ tests/test_f1score.py
@@ -0,0 +1,10 @@
+from .utils import compare_metrics
+from extra_keras_metrics import F1Score
+from sklearn.metrics import f1_score as baseline
+import numpy as np
+
+def f1score_score(y_true, y_pred):
+    return baseline(y_true, np.round(y_pred).astype(int))
+
+def test_precision():
+    compare_metrics(F1Score(), f1score_score)
---
+++ tests/test_mcc.py
@@ -0,0 +1,10 @@
+from .utils import compare_metrics
+from extra_keras_metrics import MatthewsCorrelationCoefficient
+from sklearn.metrics import matthews_corrcoef as baseline
+import numpy as np
+
+def mcc_score(y_true, y_pred):
+    return baseline(y_true, np.round(y_pred).astype(int))
+
+def test_precision():
+    compare_metrics(MatthewsCorrelationCoefficient(), mcc_score)
