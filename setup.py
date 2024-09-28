1e15ff0265e2fd82c0cc3a9b5021fc27adf391fd
Luca <cappelletti.luca94@gmail.com>
2024-09-28 08:41:22 +0200

    Resolved typo in setup

--- setup.py
+++ setup.py
@@ -10,19 +10,18 @@ from setuptools import find_packages, setup
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
-with copen(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with copen(path.join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
 
 def read(*parts):
-    with copen(os.path.join(here, *parts), 'r') as fp:
+    with copen(os.path.join(here, *parts), "r") as fp:
         return fp.read()
 
 
 def find_version(*file_paths):
     version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
     if version_match:
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
@@ -30,15 +29,23 @@ def find_version(*file_paths):
 
 __version__ = find_version("extra_keras_metrics", "__version__.py")
 
-test_deps = ["pytest", "codacy-coverage", "coveralls", "validate_version_code",
-             "pytest-cov", "numpy", "sklearn", "silence_tensorflow"]
+test_deps = [
+    "pytest",
+    "codacy-coverage",
+    "coveralls",
+    "validate_version_code",
+    "pytest-cov",
+    "numpy",
+    "sklearn",
+    "silence_tensorflow",
+]
 
 extras = {
-    'test': test_deps,
+    "test": test_deps,
 }
 
 setup(
-    name='extra_keras_metrics',
+    name="extra_keras_metrics",
     version=__version__,
     description="Additional metrics integrated with the TensorFlow and Keras NN library.",
     long_description=long_description,
@@ -46,16 +53,16 @@ setup(
     author="Luca Cappelletti",
     author_email="cappelletti.luca94@gmail.com",
     # Choose your license
-    license='MIT',
+    license="MIT",
     include_package_data=True,
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3'
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
     ],
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
     tests_require=test_deps,
     # Add here the package dependencies
-    install_requires=["decorator", "extra_keras_metrics>=1.0.2"],
+    install_requires=["decorator"],
     extras_require=extras,
 )
